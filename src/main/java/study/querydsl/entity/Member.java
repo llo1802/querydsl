34a37fe9d0c72df8be548cf23c6cc64036b0f4d0
llo1802 <llo1802@hknu.ac.kr>
2024-03-04 01:51:26 +0900

    first commit

--- src/main/java/study/querydsl/entity/Member.java
+++ src/main/java/study/querydsl/entity/Member.java
@@ -7,7 +7,6 @@ import lombok.*;
 @Getter
 @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@ToString(of = {"id", "username", "age"})
 public class Member {
 
     @Id
--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -35,7 +35,7 @@ import static org.assertj.core.api.Assertions.*;
 import static study.querydsl.entity.QMember.*;
 import static study.querydsl.entity.QTeam.*;
 
-@SpringBootTest
+@SpringBootTest(classes = QuerydslApplicationTests.class)
 @Transactional
 public class QuerydslBasicTest {
 
